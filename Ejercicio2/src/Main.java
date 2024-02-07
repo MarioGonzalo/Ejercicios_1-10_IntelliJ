3ceffd078b36b017a8bc1ce277f9a2b891bd1796
MarioGonzalo <146821965+MarioGonzalo@users.noreply.github.com>
2024-02-07 19:25:12 +0100

    Ejercicio2

---
+++ Ejercicio2/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("El mayor número representable en un char es: " + Character.MAX_VALUE);
+        System.out.println("El mayor número representable en un short es: " + Short.MAX_VALUE);
+        System.out.println("El mayor número representable en un int es: " + Integer.MAX_VALUE);
+    }
+}
