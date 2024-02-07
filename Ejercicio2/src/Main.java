641827c5e025a002d337f02a0ccf44e4e82a6241
MarioGonzalo <146821965+MarioGonzalo@users.noreply.github.com>
2024-02-07 20:18:05 +0100

    Ejercicio 2 adición de comentarios

--- Ejercicio2/src/Main.java
+++ Ejercicio2/src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
+        // Se imprime el valor máximo que puede tener cada tipo de dato primitivo empleando MAX_VALUE
         System.out.println("El mayor número representable en un char es: " + Character.MAX_VALUE);
         System.out.println("El mayor número representable en un short es: " + Short.MAX_VALUE);
         System.out.println("El mayor número representable en un int es: " + Integer.MAX_VALUE);
